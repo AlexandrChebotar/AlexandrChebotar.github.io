2a1c36d6099bb4a4bb4f663f554e2be926622a32
AlexandrChebotar <44335524+AlexandrChebotar@users.noreply.github.com>
2019-03-01 22:09:15 +0200

    add HT (with redux)

---
+++ HT7/with-redux/precache-manifest.854ac68cceca14e9bea0cccfcbd78db9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "fbc1416ed9a893667624",
+    "url": "/static/js/main.fbc1416e.chunk.js"
+  },
+  {
+    "revision": "f939b416f7e4ed0fd990",
+    "url": "/static/js/2.f939b416.chunk.js"
+  },
+  {
+    "revision": "fbc1416ed9a893667624",
+    "url": "/static/css/main.363bc74d.chunk.css"
+  },
+  {
+    "revision": "8fb4ccf57f4d0f38b28d0a954cee7006",
+    "url": "/index.html"
+  }
+];
